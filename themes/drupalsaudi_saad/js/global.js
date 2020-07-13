77ec82b7b15b06b2c0b46dc192c348fb4de66c57
shabib-m <abngeath@gmail.com>
2020-07-13 09:14:54 +0300

    new theme (drupalsaudi_saad)

---
+++ themes/drupalsaudi_saad/js/global.js
@@ -0,0 +1,16 @@
+/**
+ * @file
+ * Global utilities.
+ *
+ */
+(function ($, Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.drupalsaudi_saad = {
+    attach: function (context, settings) {
+
+    }
+  };
+
+})(jQuery, Drupal);
