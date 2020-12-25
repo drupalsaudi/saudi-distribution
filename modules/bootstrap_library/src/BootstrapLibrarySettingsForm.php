a027ff93f6ec740bc9974bae53e1c7f4cc335aee
shabib-m <abngeath@gmail.com>
2020-12-25 20:14:08 +0300

    update bootstrap_library

--- modules/bootstrap_library/src/BootstrapLibrarySettingsForm.php
+++ modules/bootstrap_library/src/BootstrapLibrarySettingsForm.php
@@ -171,6 +171,13 @@ function _bootstrap_library_data() {
   return '{
   "timestamp": "2015-11-09T18:54:50.335Z",
   "bootstrap": {
+    "4.5.2": {
+      "css": "//stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css",
+      "js": [
+        "//cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.1/umd/popper.min.js",
+        "//stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"
+      ]
+    },
     "4.4.1": {
       "css": "//stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css",
       "js": [
